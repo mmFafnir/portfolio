77deaa5861a6cbe30df1d463148287f9cda60646
om-sv <om-sv2@mail.ru>
2023-09-02 18:19:31 +0300

    add telegram

--- src/components/UI/Socials/index.tsx
+++ src/components/UI/Socials/index.tsx
@@ -5,7 +5,7 @@ const Socials:FC = () => {
     return (
         <div className="social __anim-item __active">
             <div className="social__item">
-                <a className="social__link tg" href="https://tlgg.ru/@mmfafnir"><i className="fab fa-telegram-plane"></i></a>
+                <a className="social__link tg" href="https://tlgg.ru/@ak_dav01"><i className="fab fa-telegram-plane"></i></a>
             </div>
             <div className="social__item">
                 <a className="social__link github" href="https://github.com/mmFafnir"><i className="fab fa-github"></i></a>
